a03b4ec2efddeaeaa8ba314938bbb3cb56662b50
Oleg Belousov <olegb@tomigo.com>
2014-03-27 17:14:36 +0200

    Revised requirements

---
+++ settings/staging.py
@@ -0,0 +1,12 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vita',
+        'USER': 'vita',
+        'HOST': '',
+        'PASSWORD': '',
+        'PORT': '',
+    }
+}
