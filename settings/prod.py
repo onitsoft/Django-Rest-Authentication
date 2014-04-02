df9d50deb702b058211b3fd6331b725238b45c38
OpsWorks user oleg <oleg@api1.localdomain>
2014-04-02 08:52:43 +0000

    prod settings adjustements

--- settings/prod.py
+++ settings/prod.py
@@ -3,10 +3,13 @@ from base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vita',
-        'USER': 'vita',
-        'HOST': '',
-        'PASSWORD': '',
-        'PORT': '',
+        'NAME': 'vita_auth',
+        'USER': 'vita_auth',
+        'HOST': 'db1.cww4gfvmqeyv.us-east-1.rds.amazonaws.com',
+        'PASSWORD': 'cfa5157a22b3f6184',
+        'PORT': '3306',
     }
 }
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
